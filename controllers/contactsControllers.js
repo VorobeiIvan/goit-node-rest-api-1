7a62e3f7df253de8ad3dfe3949d7bb396f6e1b9e
VorobeiIvan <0675361905vi@gmail.com>
2024-05-04 10:20:42 +0300

    added a solution for hw2

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,87 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsServices from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+import HttpError from "../helpers/HttpError.js";
 
-export const getOneContact = (req, res) => {};
+import {
+  contactAddSchema,
+  contactUpdateSchema,
+} from "../schemas/contactsSchemas.js";
 
-export const deleteContact = (req, res) => {};
+const getAll = async (req, res, next) => {
+  try {
+    const result = await contactsServices.getAllContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsServices.getContactById;
+    if (!result) {
+      throw HttpError(404, `Movie with id=${id} not found`);
+    }
 
-export const updateContact = (req, res) => {};
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const add = async (req, res, next) => {
+  try {
+    const { error } = contactAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contactsServices.addContact(req.body);
+
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateById = async (req, res, next) => {
+  try {
+    const { error } = contactUpdateSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    const { id } = req.params;
+    const result = await contactsServices.updateContactById(id, req.body);
+    if (!result) {
+      throw HttpError(404, `Movie with id=${id} not found`);
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsServices.deleteContactById(id);
+    if (!result) {
+      throw HttpError(404, `Movie with id=${id} not found`);
+    }
+    res.json({
+      message: "Delete success",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  getAll,
+  getById,
+  add,
+  updateById,
+  deleteById,
+};
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -3,46 +3,52 @@ import path from "path";
 import { nanoid } from "nanoid";
 
 const contactsPath = path.join("db", "contacts.json");
-const updateContacts = async (db) => {
-  await fs.writeFile(contactsPath, JSON.stringify(db, null, 2));
-};
+const updateContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-async function listContacts() {
+export const getAllContacts = async () => {
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
-}
+};
 
-async function getContactById(contactId) {
-  const data = await listContacts();
-  const result = data.find((item) => item.id === contactId);
+export const getContactById = async (contactId) => {
+  const contacts = await getAllContacts();
+  const result = contacts.find((item) => item.id === contactId);
   return result || null;
-}
+};
 
-async function removeContact(contactId) {
-  const data = await listContacts();
-  const index = data.findIndex((item) => item.id === contactId);
+export const addContact = async (data) => {
+  const contacts = await getAllContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+
+  return newContact;
+};
+export const updateContactById = async (id, data) => {
+  const contacts = await getAllContacts();
+  const index = contacts.findIndex((item) => item.id === id);
   if (index === -1) {
     return null;
   }
 
-  const [result] = data.splice(index, 1);
-  await updateContacts(data);
+  contacts[index] = { ...contacts[index], ...data };
+  await updateContacts(contacts);
+
+  return contacts[index];
+};
+export const deleteContactById = async (contactId) => {
+  const contacts = await getAllContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
 
   return result;
-}
-
-async function addContact(name, email, phone) {
-  const data = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-  data.push(newContact);
-  await updateContacts(data);
-
-  return newContact;
-}
-
-export { listContacts, getContactById, removeContact, addContact };
+};
